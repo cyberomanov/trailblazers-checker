74c7861e92accd3e67d55c8404b4597956111315
cyberomanov <romanoff_1@bk.ru>
2024-10-12 04:31:10 +0300

    v2.0 - claim

--- tools/executor.py
+++ tools/executor.py
@@ -5,7 +5,7 @@ from loguru import logger
 from web3 import Web3
 
 from data.constants import taiko_chain, taiko_token
-from tools.crypto import claim_taiko_tx, get_balance_of, transfer_token_tx
+from tools.crypto import claim_taiko_tx, get_balance_of, transfer_token_tx, get_balance
 from tools.other_utils import get_reward, sleep_in_range
 from user_data.config import sleep_between_accounts
 
@@ -21,30 +21,38 @@ def single_executor(index: int, line: str, session: requests.Session()):
         proof = "".join(address[2:] for address in ast.literal_eval(reward.proof))
         logger.success(f"#{index} | {address} | {value} $TAIKO.")
 
-        claim_tx = claim_taiko_tx(private_key=private_key, amount=value, proof=proof)
-        if claim_tx:
-            if "already claimed" in claim_tx:
-                logger.warning(f"#{index} | {address} | claim_tx | already claimed.")
-            else:
-                logger.info(f"#{index} | {address} | claim_tx | {taiko_chain.explorer}/{claim_tx}")
-                sleep_in_range(sec_from=sleep_between_accounts[0], sec_to=sleep_between_accounts[1])
+        taiko_eth_balance = get_balance(address=address, rpc=taiko_chain.rpc)
+        if taiko_eth_balance.float > 0.0005:
+            claim_tx = claim_taiko_tx(private_key=private_key, amount=value, proof=proof)
+            if claim_tx:
+                if "already claimed" in claim_tx:
+                    logger.warning(f"#{index} | {address} | claim_tx | already claimed.")
+                else:
+                    logger.info(f"#{index} | {address} | claim_tx | {taiko_chain.explorer}/{claim_tx}")
+                    sleep_in_range(sec_from=sleep_between_accounts[0], sec_to=sleep_between_accounts[1])
 
-        taiko_balance = get_balance_of(address=address, rpc=taiko_chain.rpc, contract=taiko_token.address)
-        if taiko_balance.int:
-            transfer_tx = transfer_token_tx(
-                private_key=private_key, amount=taiko_balance.int, recipient_address=recipient_address
-            )
-            if transfer_tx:
-                logger.info(
-                    f"#{index} | {address} | "
-                    f"transfer {taiko_balance.float} $TAIKO -> {recipient_address} | "
-                    f"{taiko_chain.explorer}/{transfer_tx}"
+            taiko_balance = get_balance_of(address=address, rpc=taiko_chain.rpc, contract=taiko_token.address)
+            if taiko_balance.int:
+                transfer_tx = transfer_token_tx(
+                    private_key=private_key, amount=taiko_balance.int, recipient_address=recipient_address
                 )
-                sleep_in_range(sec_from=sleep_between_accounts[0], sec_to=sleep_between_accounts[1], log=True)
+                if transfer_tx:
+                    logger.info(
+                        f"#{index} | {address} | "
+                        f"transfer {taiko_balance.float} $TAIKO -> {recipient_address} | "
+                        f"{taiko_chain.explorer}/{transfer_tx}"
+                    )
+                    sleep_in_range(sec_from=sleep_between_accounts[0], sec_to=sleep_between_accounts[1], log=True)
+            else:
+                logger.warning(
+                    f"#{index} | {address} | "
+                    f"transfer_tx | nothing to transfer: {taiko_balance.float} $TAIKO."
+                )
         else:
             logger.warning(
                 f"#{index} | {address} | "
-                f"transfer_tx | nothing to transfer: {taiko_balance.float} $TAIKO."
+                f"balance | not enough balance for claim. "
+                f"minimum required amount: 0.0005 $ETH, actual balance: {taiko_eth_balance.float} $ETH."
             )
 
     else:
