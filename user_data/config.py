d3480508adbd8f45453cadf1fe616388820df82f
cyberomanov <romanoff_1@bk.ru>
2024-10-17 22:48:19 +0300

    v3.0 - bridge top-up

--- user_data/config.py
+++ user_data/config.py
@@ -3,3 +3,8 @@ sleep_between_accounts = (10, 20)
 shuffle_accounts = True
 mobile_proxy = "socks5://log:pass@ip:port"
 change_ip_url = ""  # leave empty, if you have travchis
+
+deposit_from_source_chains = False  # если нет баланса на taiko, скрипт будет депозитить баланс из source_chains
+bridges_to_use = ["orbiter", "xy"]  # orbiter, xy
+deposit_to_taiko_amount = (0.0003, 0.0004)
+gas_multiplier = 2
